46bcf1bce1ba76cdc5299b0e8b0fe6abbfd29523
StrayCrowGames <140660412+StrayCrowGames@users.noreply.github.com>
2023-07-29 14:00:14 -0700

    Main Menu Skeleton
    Created a main menu with play/options/credits/exit button. The play button enters the player into the main scene. The options button opens a menu (currently empty) where the player can adjust audio, controls, and video. They can apply these settings or return back to the main menu. The credits button currently does nothing, but will eventually give credit to everyone working on the project. The exit button closes the game and window.

---
+++ Scripts/Control.gd
@@ -0,0 +1,51 @@
+extends Control
+
+#Variables to control what is displaying on the screen
+@onready var main_menu_container = %MainMenuContainer
+@onready var options_container = %OptionsContainer
+
+
+# --------- PLAY BUTTON ---------
+#--------------------------------
+
+#Switches from main menu to main scene when player presses play button
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+
+
+# --------- OPTIONS CONTROL ---------
+#------------------------------------
+
+#Hides main menu and opens options menu when options button is pressed
+func _on_options_pressed():
+	main_menu_container.visible = false
+	options_container.visible = true
+
+#Hides options menu and returns view to main menu when back button is pressed
+func _on_back_pressed():
+	main_menu_container.visible = true
+	options_container.visible = false
+
+
+
+
+# --------- CREDITS BUTTON ---------
+#-----------------------------------
+
+#Will display the game credits when credits button is pressed
+func _on_credits_pressed():
+	pass 
+
+
+
+
+
+# --------- EXIT BUTTON ---------
+#--------------------------------
+
+#Will close and exit the game when the exit button is pressed
+func _on_exit_pressed():
+	get_tree().quit()
+
