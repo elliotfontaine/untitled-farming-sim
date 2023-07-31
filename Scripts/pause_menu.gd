b46cfc2f00045fd3d226584675e14356b3d71c92
StrayCrowGames <140660412+StrayCrowGames@users.noreply.github.com>
2023-07-30 17:53:44 -0700

    Created Menu Button And Script
    Created button for Pause Menu ui and a Script for the Pause menu. Still need to create pause menu scene.

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@onready var menu_ui = %MenuUI 
+var is_open : bool = false
+
+
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		if not is_open:
+			OpenMenu()
+		else:
+			CloseMenu()
+	
+func OpenMenu():
+		is_open = true
+		menu_ui.visible = true
+		get_tree().paused = true
+		print(is_open)
+
+func CloseMenu():
+		is_open = false
+		menu_ui.visible = false
+		get_tree().paused = false
+		print(is_open)
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit
+
+
