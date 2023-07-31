0b1f17bd75126189e4e697b5b3f4b7652a306761
Elliot Fontaine <elliot.fontaine@gmail.com>
2023-07-31 19:40:06 -0400

    Refactoring of can_place_crop()

--- world/world_tilemap/world_tilemap.gd
+++ world/world_tilemap/world_tilemap.gd
@@ -8,11 +8,11 @@ extends TileMap
 ## For access by name to the different layers. Should be kept
 ## up to date if modifying the layers / their order in the inspector!
 enum LAYERS {
-	soil_data_layers,
-	dirt_layer,
-	ground_layer,
-	crops_layer,
-	obstacle_layer
+	soil_data,
+	dirt,
+	ground,
+	crops,
+	obstacles
 }
 
 ## Same thing, keep ut to date with sources in the tileset
@@ -47,16 +47,16 @@ var crops_collection_source = tile_set.get_source(SOURCE_ID.crops_collection)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# this is just to debug / test script for now
-#	erase_cell(LAYERS.ground_layer, Vector2i(11, 11))
+#	erase_cell(LAYERS.ground, Vector2i(11, 11))
 	add_crop(Vector2i(14,10), CROP_ID.potato)
 	add_crop(Vector2i(14,10), CROP_ID.carrot)
-	
+	add_crop(Vector2i(14,8), CROP_ID.potato)
 #	add_crop(Vector2i(14,11), CROP_ID.carrot)
 #	remove_crop(Vector2i(14,11))
 #
 #	add_crop(Vector2i(14,10), CROP_ID.carrot)
 #	add_crop(Vector2i(14,11), CROP_ID.carrot)
-	
+
 	pass # Replace with function body.
 
 
@@ -64,31 +64,55 @@ func _ready():
 func _process(_delta):
 	pass
 
-# Public functions
+# Public methods
 func add_crop(coords: Vector2i, crop_scene_id: int = 0):
-	if can_place(coords):
+	if can_place_crop(coords):
 		set_cell(
-			LAYERS.crops_layer,
+			LAYERS.crops,
 			coords,
 			SOURCE_ID.crops_collection,
-			Vector2i(0,0),
+			Vector2i.ZERO,
 			crop_scene_id
 		)
+	else:
+		push_warning("Invalid tile for crop placement.")
 	
 func remove_crop(coords: Vector2i) -> void:
-	erase_cell(LAYERS.crops_layer, coords)
-	
-func can_place(coords: Vector2i) -> bool:
-	
-	# Get the ID of the tile at the given coordinates from the ground layer
-	var ground_tiles = get_cell_tile_data(LAYERS.ground_layer, coords)
-	# if the ground exists, continue
-	if ground_tiles:
-		# check if a tile has been marked with "can_place" -> true
-		if is_tile_located(LAYERS.crops_layer, coords) and ground_tiles.get_custom_data("can_place"):
+	erase_cell(LAYERS.crops, coords)
+
+
+# private methods
+
+## Check if a given cell at a certain layer has a tile in it. Return true if
+## the cell is empty.
+func is_cell_empty(layer: LAYERS, coords: Vector2i) -> bool:
+	# get_cell_source_id returns -1 if a cell is empty
+	return get_cell_source_id(layer, coords) == -1
+
+## Check if given coordinates on the tilemap are a valid space to place a crop.
+## Should have no obstacle, no crop already in place, and the "can_place"
+## custom data for the tile on the ground layer.
+func can_place_crop(coords: Vector2i) -> bool:
+	if (
+		!is_cell_empty(LAYERS.ground, coords)
+		and is_cell_empty(LAYERS.obstacles, coords)
+		and is_cell_empty(LAYERS.crops, coords)
+	):
+		var ground_cell_data = get_cell_tile_data(LAYERS.ground, coords)
+		if ground_cell_data.get_custom_data("can_place"):
 			return true
 	return false
+#	# Get the ID of the tile at the given coordinates from the ground layer
+#	var ground_cell_data = get_cell_tile_data(LAYERS.ground, coords)
+#	var obstacle_cell_data = get_cell_tile_data(LAYERS.obstacle, coords)
+#	# if the ground exists, continue
+#	if ground_cell_data:
+#		# check if a tile has been marked with "can_place" -> true
+#		if is_tile_located(LAYERS.crops, coords) and ground_cell_data.get_custom_data("can_place"):
+#			return true
+#	return false
 
+# = is tile empty ?
 func is_tile_located(layer : LAYERS, coords : Vector2i) -> bool:
 	# Get a list of all the cells on a given layer
 	var cells_array = get_used_cells(layer)
