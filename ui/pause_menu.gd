59818577f81ecbc8985c77b4d577f1e75e6fd3de
StrayCrowGames <140660412+StrayCrowGames@users.noreply.github.com>
2023-08-03 20:29:39 -0700

    Created functional UI
    pause menu is now theme'd. Can open and close while pausing game. Can open the options menu. Button to toggle game music still needs work.

---
+++ ui/pause_menu.gd
@@ -0,0 +1,55 @@
+extends Control
+
+@onready var menu_ui = %MenuUI
+@onready var options_menu = %OptionsMenu
+@onready var buttons_menu = %ButtonsMenu
+#@onready var music = $"res://sounds/SoundScene.tscn"
+
+var is_open : bool = false
+
+
+#MAKES SURE PAUSE MENU IS CLOSED ON READY
+func _ready():
+	menu_ui.visible = false
+
+#ALLOWS PAUSE MENU TO BE TOGGLED WITH ESC KEY
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		if not is_open:
+			OpenMenu()
+		else:
+			CloseMenu()
+
+#MAKES PAUSE MENU VISIBLE, PAUSES GAME
+func OpenMenu():
+		is_open = true
+		menu_ui.visible = true
+		buttons_menu.visible = true
+		options_menu.visible = false
+		get_tree().paused = true
+
+#MAKES PAUSE MENU INVISIBLE, UNPAUSES GAME
+func CloseMenu():
+		is_open = false
+		menu_ui.visible = false
+		get_tree().paused = false
+
+#EXITS GAME WHEN QUIT BUTTON IS PRESSED
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+#OPENS OPTIONS MENU
+func _on_options_pressed():
+	options_menu.visible = true
+	buttons_menu.visible = false
+
+#TOGGLES GAME MUSIC ON AND OFF
+func _on_music_toggle_toggled(button_pressed):
+	#if button_pressed:
+		#music.stream.paused()
+
+#RETURNS OPTION MENU BACK TO MAIN PAUSE MENU
+func _on_back_button_pressed():
+	options_menu.visible = false
+	buttons_menu.visible = true
