bcd2f39e1ecf43830920ffdcb5c78f10e91d73e0
Curly Code <68510545+Culiynl@users.noreply.github.com>
2023-08-05 16:15:29 -0700

    fixed cow walking animation and sped cow up
    Also added cow mooing at random times

--- Cow.gd
+++ Cow.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
 @export var wander_range : float = 5
-@export var speed : int = 2
+@export var speed : int = 5
 
 var currentPos : Vector2
 var wanderPos : Vector2
 
-var cow_left = preload("res://animals/cow-left.png")
-var cow_right = preload("res://animals/cow-right.png")
 @onready var animation_player = $AnimationPlayer
 @onready var sprite_2d = $Sprite2D
 
@@ -21,11 +19,13 @@ func _physics_process(delta):
 	if move_and_slide():
 		wanderPos = Vector2(-wanderPos.x, -wanderPos.y)
 	if wanderPos.x < 0:
-		sprite_2d.texture = cow_left
-		animation_player.play("cow_left")
-	else:		
-		sprite_2d.texture = cow_right
-		animation_player.play("cow_right")
+		sprite_2d.set_flip_h(false)
+		animation_player.play("cow")
+	elif wanderPos.x > 0:		
+		sprite_2d.set_flip_h(true)
+		animation_player.play("cow")
+	if randi() % 600 == 0:
+		SoundHandler.play_sound(4)
 
 func get_new_wander_dir():
 	var target_vector = Vector2(randf_range(-wander_range, wander_range),randf_range(-wander_range, wander_range))
