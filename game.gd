4f9f4f31298f5f7fd04e025e9ebcce132fdc31f2
Elliot Fontaine <elliot.fontaine@gmail.com>
2023-08-10 17:47:56 -0400

    Change the random seed at game startup.

--- game.gd
+++ game.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var current_level = $TitleScreen
 
 func _ready() -> void:
+	randomize()
 	current_level.level_changed.connect(switch_scene)
 ## Takes filepath to a Scene and replace the current level by a new one.
 
