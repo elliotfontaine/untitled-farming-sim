c693c17469ff4fd504403ffe7e95b5ef7319e484
Elliot Fontaine <elliot.fontaine@gmail.com>
2023-09-04 20:07:01 +0200

    buy/sell buttons added

--- ui/inventories/hot_bar.gd
+++ ui/inventories/hot_bar.gd
@@ -1,14 +1,15 @@
 extends Control
 
-
 @export var player: Node2D
 
 var normal_slot = preload("res://ui/assets/item_slot.png")
 var clicked_slot = preload("res://ui/assets/clicked_slot.png")
 var has_player: bool = false
-var slots: Array
 
-@onready var grid_container = $NinePatchRect/GridContainer
+@onready var grid_container := $SlotsRect/SlotsGrid
+@onready var buy_label := $ShopRect/BuyButton/Label
+@onready var sell_label := $ShopRect/SellButton/Label
+@onready var slots := grid_container.get_children()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,15 +17,14 @@ func _ready():
 		has_player = true
 	else:
 		print("No player inventory attached.")
-	slots = grid_container.get_children()
 	select_slot(0)
 
-func reset_slots():
+func reset_slot_selection():
 	for x in range(slots.size()):
 		slots[x].set_texture_normal(normal_slot)
 		
 func select_slot(x : int):
-	reset_slots()
+	reset_slot_selection()
 	slots[x].set_texture_normal(clicked_slot)
 	if has_player:
 		player.selected_crop = player.crop_strnames[x]
@@ -32,38 +32,29 @@ func select_slot(x : int):
 func _on_beet_slot_pressed():
 	select_slot(0)
 
-
 func _on_potato_slot_pressed():
 	select_slot(1)
-
 
 func _on_tomato_slot_pressed():
 	select_slot(2)
 
-
 func _on_french_beans_slot_pressed():
 	select_slot(3)
-
 
 func _on_pumpkin_slot_pressed():
 	select_slot(4)
 
-
 func _on_carrot_slot_pressed():
 	select_slot(5)
-
 
 func _on_onion_slot_pressed():
 	select_slot(6)
 
-
 func _on_cauliflower_slot_pressed():
 	select_slot(7)
 
-
 func _on_turnip_slot_pressed():
 	select_slot(8)
-
 
 func _on_wheat_slot_pressed():
 	select_slot(9)
